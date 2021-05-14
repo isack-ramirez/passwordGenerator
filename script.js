afc1b890a3e145ab213f8fc25bf60b8960bf0b69
Isack Ramirez <isack.ramirez1@gmail.com>
2021-05-14 17:41:13 -0500

    Added option for user to select password length, and implemented vallidation to check both length and if it is an integer.

--- script.js
+++ script.js
@@ -1,73 +1,104 @@
 
-var charArrary=['a','b','c','d','e','f','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numArrary=[1,2,3,4,5,6,7,8,9,0];
-var SPcharArray=['$','%',"@",'!','&']
+var charArray = ['a', 'b', 'c', 'd', 'e', 'f', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var SPcharArray = ['$', '%', "@", '!', '&', '"', '#',"'","(",")","*","+",",","-",".","/",":",";"
+,"<","=",">","?","[","]","^","_","`","|","}"," ~"]
 
-var generatedPassword=[];
+var generatedPassword = [];
 
-var useChars = true;
-var useNums = true;
-var useSPchars = true;
+var useChars = false;
+var useNums = false;
+var useSPchars = false;
 
-var validated=false;
+var validated = false;
 
-while(validated===false){
-  var usingChars=prompt("Hello! Would you like to use [LETTERS] in your password? Y / N ")
-      if(usingChars==='y' ||usingChars=== 'Y'){
-          usechars=true;
-          validated=true;
-      }
-    else if(usingChars==='n' ||usingChars=== 'N'){
-      useChars=false;
-      validated=true;
-  
-    }
-    else{
-      alert("Invalid input! Please enter the letter Y or N.")
-    }
-  
+while (validated === false) {
+  var usingChars = prompt("Hello! Would you like to use [LETTERS] in your password? Y / N ")
+  if (usingChars === 'y' || usingChars === 'Y') {
+    useChars = true;
+    validated = true;
+  }
+  else if (usingChars === 'n' || usingChars === 'N') {
+    useChars = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
   }
 
-validated = false;
-
-while(validated===false){
-
-   var usingNums=prompt("Hello! Would you like to use [NUMBERS] in your password? Y / N ")
-   if(usingNums==='y' ||usingNums=== 'Y'){
-    useNums=true;
-    validated=true;
-}
-else if(usingNums==='n' ||usingNums=== 'N'){
-useNums=false;
-validated=true;
-
-}
-else{
-alert("Invalid input! Please enter the letter Y or N.")
-}
-
 }
 
 validated = false;
 
-while(validated===false){
+while (validated === false) {
 
-   var usingSPchars=prompt("Hello! Would you like to use [SPECIAL CHARACTERS (eg % )] in your password? Y / N ")
-   if(usingSPchars==='y' ||usingSPchars=== 'Y'){
-    useSPchars=true;
+  var usingNums = prompt("Hello! Would you like to use [NUMBERS] in your password? Y / N ")
+  if (usingNums === 'y' || usingNums === 'Y') {
+    useNums = true;
+    validated = true;
+  }
+  else if (usingNums === 'n' || usingNums === 'N') {
+    useNums = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
+  }
+
+}
+
+validated = false;
+
+while (validated === false) {
+
+  var usingSPchars = prompt("Hello! Would you like to use [SPECIAL CHARACTERS (eg % )] in your password? Y / N ")
+  if (usingSPchars === 'y' || usingSPchars === 'Y') {
+    useSPchars = true;
+    validated = true;
+  }
+  else if (usingSPchars === 'n' || usingSPchars === 'N') {
+    use = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
+  }
+
+}
+
+
+validated = false;
+var passwordLength;
+while (validated === false) {
+
+  var passwordLength = prompt("Enter the number of characters desired (min 8 max 128)");
+  
+  isLetter=isNaN(passwordLength);
+
+  if(passwordLength < 8 ) {
+    alert("Please enter a number between 8 and 128")
+    passwordLength=0;
+
+  }
+
+  else if(passwordLength > 128 ){
+    alert("Please enter a number between 8 and 128")
+    passwordLength=0;
+  }
+ 
+  else if(isLetter===true){
+    alert("Please enter a number between 8 and 128")
+    passwordLength=0;
+  }
+
+  else{
     validated=true;
-}
-else if(usingSPchars==='n' ||usingSPchars=== 'N'){
-use=false;
-validated=true;
+  }
 
 }
-else{
-alert("Invalid input! Please enter the letter Y or N.")
-}
-
-}
-
 
 
 
@@ -81,14 +112,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function writePassword() {
-  generatedPassword=[];
+  generatedPassword = [];
 
   var passwordText = document.querySelector("#password");
 
-
-
- console.log("button pressed")
-
+  console.log("button pressed")
 
   password = generatePassword();
 
@@ -118,63 +146,36 @@ Write to the screen
 
 
 
-function generatePassword(){
+function generatePassword() {
 
-      if(useChars===true && useNums===true && useSPchars===true){
-          
-          var combinedArrays = charArrary.concat(numArrary,SPcharArray);
-      }
+  console.log(useChars);
+  console.log(useNums);
+  console.log(useSPchars);
 
-      else if(useChars===true && useNums===true && useSPchars===false){
-        var combinedArrays = charArrary.concat(numArrary);
-      }
+  var combinedArrays = [];
+  if (useChars === true) {
 
-      else if(useChars===true && useNums===true && useSPchars===false){
-        var combinedArrays = charArrary.concat(numArrary);
-      }
-      
-      else if(useChars===true && useNums===false && useSPchars===true){
-        var combinedArrays = charArrary.concat(SPcharArray);
-      } 
+    combinedArrays = combinedArrays.concat(charArray);
+    console.log(combinedArrays);
+  
+  }
+  if (useNums === true) {
+    combinedArrays = combinedArrays.concat(numArray);
+  }
 
-      else if(useChars===true && useNums===false && useSPchars===false){
-        var combinedArrays = charArrary
-      } 
+  if (useSPchars === true) {
+    combinedArrays = combinedArrays.concat(SPcharArray);
+  }
 
-      else if(useChars===false && useNums===true && useSPchars===true){
-        var combinedArrays = numArrary.concat(SPcharArray);
-      } 
+  for (i = 0; i < passwordLength; i++) {
+    console.log();
 
-      else if(useChars===false && useNums===false && useSPchars===true){
-        var combinedArrays = SPcharArray;
-      } 
+    generatedPassword.push(combinedArrays[Math.floor(Math.random() * combinedArrays.length)]);
 
-      else if(useChars===false && useNums===true && useSPchars===false){
-        var combinedArrays = numArrary;
-      } 
-
-      else if(useChars===false && useNums===false && useSPchars===true){
-        var combinedArrays = SPcharArray;
-      } 
-
-      else{
-        combinedArrays=['H',"H","H","A","A","HA"]
-      }
-
-      
+  } return generatedPassword;
 
 
 
-
-
-          for(i=0;i<=3;i++){
-                console.log();
-                generatedPassword.push(combinedArrays[Math.floor(Math.random()*combinedArrays.length)]);
-                
-          }return generatedPassword;
-      
-
-    
 
 
 
@@ -183,11 +184,12 @@ function generatePassword(){
 
 
 
-function getRandomItemFromArray(arrayX){
-    
-    return arrayX[Math.floor(Math.random()*arrayX.length)];
+function getRandomItemFromArray(arrayX) {
+
+  return arrayX[Math.floor(Math.random() * arrayX.length)];
 
 }
+
 
 console.log(generatedPassword);
 
@@ -195,7 +197,7 @@ console.log(generatedPassword);
 
 
 
-    
+
 
 
 
