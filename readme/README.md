d781965dae908d6608614048ea624cbca65c5362
Isack Ramirez <isack.ramirez1@gmail.com>
2021-05-15 14:28:36 -0500

    updated readme

---
+++ readme/README.md
@@ -0,0 +1,46 @@
+# passwordGenerator
+
+This is a simple, lightweight password generator:
+
+Default javascript prompt boxes ask a user a series of questions:
+
+    "Do you want to use alphabetic characters?"
+    "Do you want to use numeric characters?"
+    "Do you want to use special characters?"
+    "How many characters would you like your password to be?"
+
+It does this using a few methods explained below in combonation:
+
+--------------------------------------------------------------------------------------------------------------------------
+
+🔴generatePassword():
+
+    No arguments when calling the method
+
+    for debugging purposes, it logs to the console which of the choices the user made when asked in the prompts
+
+    It then runs through a series of if statements checking the boolean value of the variables useChoice, useNums, and useSpchars
+
+    if the value of any of these is true it is concantinated with local-scope array called combinedArrays
+
+    after these checks are run it runs a for loop based on the user input for how desired length of the password, stored in a global variable passwordLength, selecting a random element from the array using the 🔴getRandomItemFromArray() method and using the 🔴push() method to append it to the end.
+
+
+🔴getRandomItemFromArray()
+
+    Takes one argument (in this case to generate the password, it draws the locally concantinated array from the 🔴generatePassword() method) and simply returns arrayX[Math.floor(Math.random() * arrayX.length)];
+    thus picking one random array element.
+
+🔴writePassword()
+
+    Stores the queary selected #password text area in the variable passwordText, for debugging purposes, logs that the button has been pressed, then stores the output of the 🔴generatePassword() in the variable password. then runs password.join(''); to remove commas when the array is displayed in the textbox. 
+
+
+There is also an event listener attached to the button @ line 125.
+
+The way user input is validated for the prompts is with a series if while loops, the variable validated is set to false initially, and after each while loop is completed until all prompts inputs have been vallidated.
+
+they are vallidated by a series of if / if else / else statements that check if the user input a value that is vallid with the questions given options, if its not then it throws an alert asking the user to use a valid input, and repeats the loop.
+
+
+
---
+++ script.js
@@ -0,0 +1,201 @@
+
+var charArray = ['a', 'b', 'c', 'd', 'e', 'f', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var SPcharArray = ['$', '%', "@", '!', '&', '"', '#', "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";"
+  , "<", "=", ">", "?", "[", "]", "^", "_", "`", "|", "}", " ~"]
+var passwordLength;
+var generatedPassword = [];
+var useChars = false;
+var useNums = false;
+var useSPchars = false;
+var validated = false;
+
+while (validated === false) {
+  var usingChars = prompt("Hello! Would you like to use [LETTERS] in your password? Y / N ")
+  if (usingChars === 'y' || usingChars === 'Y') {
+    useChars = true;
+    validated = true;
+  }
+  else if (usingChars === 'n' || usingChars === 'N') {
+    useChars = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
+  }
+
+}
+
+validated = false;
+
+while (validated === false) {
+
+  var usingNums = prompt("Hello! Would you like to use [NUMBERS] in your password? Y / N ")
+  if (usingNums === 'y' || usingNums === 'Y') {
+    useNums = true;
+    validated = true;
+  }
+  else if (usingNums === 'n' || usingNums === 'N') {
+    useNums = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
+  }
+
+}
+
+validated = false;
+
+while (validated === false) {
+
+  var usingSPchars = prompt("Hello! Would you like to use [SPECIAL CHARACTERS (eg % )] in your password? Y / N ")
+  if (usingSPchars === 'y' || usingSPchars === 'Y') {
+    useSPchars = true;
+    validated = true;
+  }
+  else if (usingSPchars === 'n' || usingSPchars === 'N') {
+    use = false;
+    validated = true;
+
+  }
+  else {
+    alert("Invalid input! Please enter the letter Y or N.")
+  }
+
+}
+
+
+validated = false;
+
+while (validated === false) {
+
+  var passwordLength = prompt("Enter the number of characters desired (min 8 max 128)");
+
+  isLetter = isNaN(passwordLength);
+
+  if (passwordLength < 8) {
+    alert("Please enter a number between 8 and 128")
+    passwordLength = 0;
+
+  }
+
+  else if (passwordLength > 128) {
+    alert("Please enter a number between 8 and 128")
+    passwordLength = 0;
+  }
+
+  else if (isLetter === true) {
+    alert("Please enter a number between 8 and 128")
+    passwordLength = 0;
+  }
+
+  else {
+    validated = true;
+  }
+
+}
+
+
+
+var combinedArrays;
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+
+function writePassword() {
+
+  var passwordText = document.querySelector("#password");
+
+  console.log("button pressed")
+
+  password = generatePassword();
+
+  passwordText.value = password.join('');
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+/* TO DO---------------------------------------------------------------------------
+
+Give user prompt for password chriteria
+: length (min 8, max 128)
+: character types (uppercase, lowercase , special characters, numeric)
+
+Vallidate user input
+
+Write to the screen
+
+
+---------------------------------------------------------------------------*/
+
+
+
+
+
+
+function generatePassword() {
+
+  console.log(useChars);
+  console.log(useNums);
+  console.log(useSPchars);
+
+  var combinedArrays = [];
+  if (useChars === true) {
+
+    combinedArrays = combinedArrays.concat(charArray);
+    console.log(combinedArrays);
+
+  }
+  if (useNums === true) {
+    combinedArrays = combinedArrays.concat(numArray);
+  }
+
+  if (useSPchars === true) {
+    combinedArrays = combinedArrays.concat(SPcharArray);
+  }
+
+  for (i = 0; i < passwordLength; i++) {
+    console.log();
+
+    generatedPassword.push(combinedArrays[Math.floor(Math.random() * combinedArrays.length)]);
+
+  } return generatedPassword;
+
+
+
+
+
+
+};
+
+
+
+
+function getRandomItemFromArray(arrayX) {
+
+  return arrayX[Math.floor(Math.random() * arrayX.length)];
+
+}
+
+
+console.log(generatedPassword);
+
+
+
+
+
+
+
+
+
+
